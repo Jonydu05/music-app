a80749aa30311ede9eb871b0da403ef9ecc3df9f
João Cardoso <joaog.zamp@gmail.com>
2025-02-06 17:55:06 -0300

    initial config

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Hi, i'm João Cardoso</h1>
+    <h2>Hi, i'm João Cardoso</h2>
+  </main>
+</template>
+
+<style lang='scss' scoped>
+h1,
+h2 {
+  font-size: 4rem;
+  font-weight: 500;
+}
+
+h1 {
+  font-family: var(--font-jost);
+}
+
+h2 {
+  font-family: var(--font-playfair);
+}
+</style>
